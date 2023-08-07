62d1b13a0154896a385460b288ea68bc60ecaf00
HmUmaxKhan <mukhan.bese22seecs@seecs.edu.pk>
2023-08-07 07:18:10 +0500

    DilyNetwork

--- src/Components/News.js
+++ src/Components/News.js
@@ -2,14 +2,14 @@ import React, { useState, useEffect } from "react";
 import NewsItem from "./NewsItem";
 import Loader from "./Loader.js";
 import PropTypes from 'prop-types'
-//  import '../Components/var';
+import InfiniteScroll from 'react-infinite-scroll-component'
 
 export default function News(props) {
   const [news, setNews] = useState([]);
   const [load, setLoad] = useState(true);
-  const [page, setPage] = useState(1);
+  const [page] = useState(1);
   const [total, setTotal] = useState(1);
-  const [pageSize] = useState(30);
+  const [pageSize,setPageSize] = useState(15);
 
 
   async function fetchItems() {
@@ -21,8 +21,8 @@ export default function News(props) {
     console.log(data);
     const articles = data.articles
     let total = data.totalResults;
-    console.log(total);
     setTotal(total);
+    setPageSize(total);
     setNews(articles);
     setLoad(false);
     
@@ -32,25 +32,39 @@ useEffect(() => {
    fetchItems();
 },)
 
-
-  const handleNext = ()=>{
-    const nextPage = page+1
-    setPage(nextPage);
-  }
-  const handlePrevious = ()=>{
-    const prevPage = page-1
-    setPage(prevPage);
-  }
+ 
   
+  const  fetchMore = async() => {
+    let info = await fetch(
+      `https://newsapi.org/v2/top-headlines?country=us&category=${props.category}&page=${page}&pageSize=${pageSize}&apiKey=${props.apikey}`
+       );
+ 
+     let data = await info.json();
+     const articles = data.articles
+     let total = data.totalResults;
+     setTotal(total);
+     const newArticles = [...news, ...articles];
+     console.log(newArticles);
+     setNews(newArticles);
+     setLoad(false);
+  }
+
+
 
   return (
     <div>
     {!load ?<div><h1 className="text-center my-2">NEWS</h1>
 
+    <InfiniteScroll
+    dataLength = {news.length}
+    next = {fetchMore}
+    hasMore = {news.length !== total}
+    loader={<div  className="d-flex justify-content-center"><Loader /></div>}
+  >
       <div className="container d-flex justify-content-around">
         <div className="row my-2 ">
 
-          {news&&news.map((newItem,index) => {
+          {news.map((newItem,index) => {
             return (
              <div key={index} className="col-md-4 my-2">
                 <NewsItem
@@ -65,13 +79,10 @@ useEffect(() => {
 
         </div>
       </div> 
+      </InfiniteScroll>
     </div>: <div className="d-flex justify-content-center" style={{height:"100vh"}}><div className="d-flex align-items-center"><Loader /></div></div>
         }
 
-        <div className="d-flex justify-content-around mb-3">
-        <button type="button" className="btn btn-dark" disabled={page<1} onClick={handlePrevious}>Previous</button>
-        <button type="button" className="btn btn-dark" disabled={page>=Math.ceil(total/pageSize)}  onClick={handleNext}>Next</button>
-        </div>
     </div>
 
   );
